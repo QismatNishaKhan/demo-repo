27f3352cea0c071be89ff55d867a9f5d200d2321
QismatNishaKhan <qismatnisha6243@gmail.com>
2022-10-17 20:07:43 +0530

    implemented HelloWord project

---
+++ HelloWord/HelloWord/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloWord
+{
+    //contructor,varaibles,prop,methods
+    class Emp
+    {
+        //declare=assign
+        //accessmodifier returntype methedname(parms)
+        public char values;
+
+        public Emp()
+        {
+            values= 'A'
+            Console.WriteLine(values);
+        }
+        
+    }
+}
---
+++ HelloWord/HelloWord/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace HelloWord
+{
+    /// <summary>
+    /// this is method line
+    /// </summary>
+    internal class Program
+    {
+        
+        public static void methed()
+        {
+
+
+
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            method();
+        }
+    }
+}
