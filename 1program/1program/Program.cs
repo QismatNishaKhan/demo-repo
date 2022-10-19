bc06dbfdf63d2f0f8d06a8fd6052e56c19988588
QismatNishaKhan <qismatnisha6243@gmail.com>
2022-10-19 18:44:10 +0530

    student information

---
+++ 1program/1program/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _1program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s = new Student(1, "qismat");
+            string value = s.GetStudentName();
+            Console.WriteLine("value1");
+        }
+        public class Student
+        {
+            public int Id;
+            public String Name;
+            
+
+            public Student(int id ,string name)
+            {
+                Id = id;
+                Name = name;
+            }
+            public string GetStudentName()
+            {
+                return Name;
+            }
+        }
+    }
+}
